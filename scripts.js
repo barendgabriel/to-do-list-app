77169d2ff833273829e89c5162ba781b93e05ecf
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-10-10 17:26:59 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {
-  // Function to add a new item
+  // Function to add a new item to the list
   function newItem() {
-    let itemText = $('#input').val(); // Get the input value
-    if (itemText) {
-      let li = $('<li></li>').text(itemText); // Create a new list item
-      let deleteButton = $('<button class="crossOutButton">X</button>'); // Create delete button
-      li.append(deleteButton); // Append the delete button to the list item
-      $('#list').append(li); // Append the new list item to the list
+    const inputValue = $('#input').val(); // Get the value from the input field
+    if (inputValue === '') {
+      alert('You must write something!'); // Alert if input is empty
+    } else {
+      // Create a new list item with a delete button
+      const listItem = $('<li></li>')
+        .text(inputValue)
+        .append('<button class="crossOutButton">X</button>');
+      $('#list').append(listItem); // Add the new item to the list
       $('#input').val(''); // Clear the input field
     }
   }
+
   // Add new item on button click
   $('#button').on('click', function () {
     newItem();
   });
+
   // Cross out item on double-click
   $('#list').on('dblclick', 'li', function () {
     $(this).toggleClass('strike'); // Toggle the strike class
   });
+
   // Delete item on button click
   $('#list').on('click', '.crossOutButton', function () {
     $(this).parent().remove(); // Remove the parent list item
   });
+
   // Make the list sortable (drag-and-drop)
   $('#list').sortable(); // Use jQuery UI to make the list sortable
 });
